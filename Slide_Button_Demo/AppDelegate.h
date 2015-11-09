f835bb1b33ba4dc85b731e0bf752d23cfcee2da9
姚隆康 <584379066@qq.com>
2015-11-09 09:13:49 +0800

    Initial Commit

---
+++ Slide_Button_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Slide_Button_Demo
+//
+//  Created by bioongroup on 15/11/9.
+//  Copyright © 2015年 ylk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
